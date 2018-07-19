5e82733435af3ddd06609467991e6fa4646b545b
@vashtryder <nice.forever.0321@gmail.com>
2018-07-19 18:58:06 -0500

    Initial commit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -44,6 +44,7 @@
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
+import 'core-js/es6/reflect';
 
 
 /**
@@ -72,7 +73,6 @@ import 'core-js/es7/reflect';
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
 
 
 /***************************************************************************************************
