5b1717629d7de84d2feceb3d8003afb897359277
@vashtryder <nice.forever.0321@gmail.com>
2018-07-19 18:40:27 -0500

    importacion de servicies

--- src/app/components/admin/m-admin-modulo/index.ts
+++ src/app/components/admin/m-admin-modulo/index.ts
@@ -1,2 +1,6 @@
 export * from '../../../_models/admin/modulo';
 export * from '../../../_services/admin/admin-modulo.service';
+export * from '../m-admin-unidad/index';
+export * from '../m-admin-curso/index';
+export * from '../../../_services/admin-global';
+
